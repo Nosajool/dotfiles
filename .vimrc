fdebfd4a1bbdbe2d2c0c72d6b5eb4f0f6c3dc6b6
Jason Loo <loo.jason95@gmail.com>
2015-09-13 18:22:11 -0400

    vim, zsh, git config and install script

---
+++ .vimrc
@@ -0,0 +1,8 @@
+set hls            " highlight search matches. unhighlight with :noh
+syntax on          " turn on syntax highlighting
+set number         " turn on line numbers
+set expandtab      " insert space characters wherever the tab key is pressed
+set tabstop=2      " 2 space characters when tab key is pressed
+set shiftwidth=2   " 2 space characters for indentation >> <<
+set softtabstop=2  " backspace treats 2 spaces as a tab and goes back 2 spaces
+set autoindent     " use indent from previous line
---
+++ install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+####################### Instructions
+# Must have git installed
+# Change into the dotfiles directory, make install.sh an executable and run the script!
+# 1. cd ~/dotfiles
+# 2. chmod +x install.sh
+# 3. ./install.sh
+
+####################### What the script does
+# 1. Clean up existing symlinks in home directory and put in ~/dotfiles_old
+# 2. Adds symlinks for dotfiles in ~/dotfiles from home directory to these files
+# http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
+#######################
+
+dir=~/dotfiles         # dotfiles directory
+olddir=~/dotfiles_old  # old dotfiles backup directory
+files=".vimrc .zshrc .oh-my-zsh"         # list of files/folders to symlink in homedir
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir       # -p creates intermediate directories if required
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing into $dir directory"
+cd $dir
+echo "...done"
+
+install_zsh () {
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then # Test if zshell is installed
+  if [[ ! -d ~/.oh-my-zsh/ ]]; then       # Check if oh-my-zsh directory exists
+    git clone http://github.com/robbyrussell/oh-my-zsh.git
+    "Renaming oh-my-zsh directory to .oh-my-zsh"
+    mv -v oh-my-zsh .oh-my-zsh
+  else
+    echo "oh-my-zsh directory already exists"
+  fi
+
+  if [[ ! $(echo $SHELL) == $(which zsh) ]]; then # Ensure default shell is zsh
+    echo "Changing login shell to use zsh"
+    chsh -s $(which zsh)                          # Specify login shell to be zsh
+  else
+    echo "Login shell is already zsh"
+  fi
+else
+  echo "Please install zsh with brew install zsh, then re-run this script!"
+  exit
+fi
+}
+
+install_zsh
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+  # http://www.gnu.org/software/bash/manual/html_node/Bash-Conditional-Expressions.html
+  if [ ! -f ~/$file ]; then # if file does not exist and files is regular 
+    echo "No existing $file found"
+  else
+    echo "Moving any existing $file from to $olddir"
+    mv -v ~/$file ~/dotfiles_old/
+  fi
+  echo "Creating symlink to $file in home directory."
+  ln -sv $dir/$file ~/$file   # -s symbolic link
+done
+
